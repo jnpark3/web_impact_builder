c126990c49e1427e6599578cb4f07af693824cb6
Jian Park <jianpark@Jians-MacBook-Pro.local>
2023-03-26 00:26:37 -0500

    Rudimentary Strategy Creator

--- frontend/src/app/creator/component_types.js
+++ frontend/src/app/creator/component_types.js
@@ -1,21 +1,43 @@
 const componentContainer = require('./component_class')
 
+const lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
 var c1_1 = new componentContainer(
     'Title & Description',
-    [[20, 61, 147, 28], [207, 43, 147, 9], [207, 43+19, 147, 9], [207, 43+38, 147, 9], [207, 43+57, 147, 9]],
-    '<div><div>#input_0#</div><div>#input_1#</div><div>',
-    ['Placeholder', 'Placeholder text'],
+    [[20, 61, 147, 28], [207, 43, 147, 9], [207, 43 + 19, 147, 9], [207, 43 + 38, 147, 9], [207, 43 + 57, 147, 9]],
+    '<div style="width: 100%;min-height: 250px;background-color: #primary_color#;display: flex;flex-direction: row;align-items: center;">\n<div style="padding: 40px; width: 40%; height: fit-content;">\n<div style="color: #tertiary_color#; font-weight: bold; font-size: 36px; display: flex; flex-direction: row; align-items: center;">#input_0#\n</div>\n</div>\n<div style="padding: 40px; width: 60%; height: fit-content; display: flex; flex-direction: row; align-items: center;">\n<div style="color: #tertiary_color#; font-size: 16px">#input_1#\n</div>\n</div>\n</div>',
+    ['Change the world one step at a time.', lorem],
     ['Header', 'Body Text']
 )
 
 var c1_2 = new componentContainer(
     'Title & Description Clone',
-    [[20, 61, 147, 28], [207, 43, 147, 9], [207, 43+19, 147, 9], [207, 43+38, 147, 9], [207, 43+57, 147, 9]],
+    [[20, 61, 147, 28], [207, 43, 147, 9], [207, 43 + 19, 147, 9], [207, 43 + 38, 147, 9], [207, 43 + 57, 147, 9]],
     '<div><div>#input_0#</div><div>#input_1#</div><div>',
     ['Placeholder 2', 'Placeholder text'],
     ['Header', 'Body Text']
 )
 
-var clist = [[c1_1, c1_2]]
+var c_1 = [c1_1, c1_2]
+
+var c2_1 = new componentContainer(
+    'Image Flat',
+    [[72, 31, 124, 89], [212, 56, 89, 64]],
+    '<div style="height: 300px; width: 100%; background-color:#primary_color#">\n</div>',
+    [{}],
+    ['Image']
+)
+
+var c_2 = [c2_1]
+
+var c_3 = []
+
+var c_4 = []
+
+var c_5 = []
+
+var c_6 = []
+
+var clist = [c_1, c_2, c_3, c_4, c_5, c_6]
 
 module.exports = clist
--- frontend/src/app/creator/creator.component.ts
+++ frontend/src/app/creator/creator.component.ts
@@ -1,10 +1,11 @@
 import { Component } from "@angular/core";
 import { AppService } from "../app.service";
 import { ActivatedRoute, Router } from "@angular/router";
-import { faArrowLeft, faImage, faCircleDot } from '@fortawesome/free-solid-svg-icons';
+import { faArrowLeft, faImage, faCircleDot, faTrash, faSave, faFileText, faVideo, faTextHeight, faMoneyBill } from '@fortawesome/free-solid-svg-icons';
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 const ComponentTypes = require('./component_types');
 const ComponentClass = require( './component_class')
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: "creator-root",
@@ -13,26 +14,32 @@ const ComponentClass = require( './component_class')
   template: '<div *ngFor="let block of html_components" [innerHTML]="block"></div>',
 })
 export class CreatorComponent {
+  save_icon = faSave
   back_icon = faArrowLeft
-  icon_1 = faImage
-  icon_2 = faCircleDot
+  icon_1 = faTextHeight
+  icon_2 = faImage
+  icon_3 = faVideo
+  icon_4 = faFileText
+  icon_5 = faMoneyBill
+  icon_6 = faCircleDot
+  trash_icon = faTrash
 
   id: number = -1;
   name: string = "";
   status: string = "";
   date: string = "";
-  settings: {} = {};
+  settings = {primary_color: '#000000', secondary_color: '#F4793E', tertiary_color: '#FFFFFF', font: 'Roboto, "Helvetica Neue", sans-serif'};
 
   loading: boolean = true;
   category_id = 0;
 
   component_list = ComponentTypes[0]
   website_components: any[] = [];
-  html_components: string[] = []
-  category_names = ['Headers', 'Images', 'Videos']
+  html_components: SafeHtml[] = []
+  category_names = ['Text', 'Images', 'Videos', 'Surveys', 'Donations', 'Miscellaneous']
 
-  constructor(private appService: AppService, private router: Router, private route: ActivatedRoute) {
-    console.log(ComponentTypes)
+  constructor(private sanitizer: DomSanitizer, private appService: AppService, private router: Router, private route: ActivatedRoute) {
+
     this.appService.verify().pipe().subscribe((data: any) => {
         if(!data.success){
           this.router.navigate(['/login'])
@@ -48,14 +55,23 @@ export class CreatorComponent {
               this.name = data2.payload.name
               this.status = data2.payload.status
               this.date = data2.payload.date
-              this.settings = data2.payload.settings
+              //this.settings = data2.payload.settings
               this.loading = false;
+              this.changeType(0)
           });
         })
     });
   }
 
-  drop(event: CdkDragDrop<string[]>) {
+  onFileSelected(event: any, id: number) {
+    const file = event.target.files[0];
+    this.editor_inputs[id] = file
+    // Do something with the file
+  }
+
+  drop(event: CdkDragDrop<SafeHtml[]>) {
+    console.log("SETTINGS:")
+    console.log(this.settings)
     if (event.previousContainer === event.container){
       var item = this.html_components[event.previousIndex];
       this.html_components[event.previousIndex] = this.html_components[event.currentIndex];
@@ -69,7 +85,13 @@ export class CreatorComponent {
       const copiedItem = item.copy();
 
       this.website_components.splice(event.currentIndex, 0, copiedItem);
-      this.html_components.splice(event.currentIndex, 0, copiedItem.generate_html(this.settings));
+      this.html_components.splice(event.currentIndex, 0, this.sanitizer.bypassSecurityTrustHtml(copiedItem.generate_html(this.settings)));
+    }
+  }
+
+  regenerate_html(){
+    for(let i = 0; i < this.website_components.length; i++){
+      this.html_components[i] = this.sanitizer.bypassSecurityTrustHtml(this.website_components[i].generate_html(this.settings))
     }
   }
 
@@ -79,10 +101,74 @@ export class CreatorComponent {
 
   changeType(id:number){
     this.category_id = id
+    this.component_list = ComponentTypes[id]
+    for(let i = 0; i < 6; i++){
+      var button = document.getElementById(`button_${i}`);
+      if(button){
+        button.style.backgroundColor = "#FFF";
+      }
+    }
+    var button = document.getElementById(`button_${id}`);
+    if(button){
+      button.style.backgroundColor = "#AAA";
+    }
   }
 
   backToMenu(){
     this.save();
     this.router.navigate(["/menu"]);
+  }
+
+  editor_inputs: any[] = [];
+  editor_input_names = [];
+  editor_id: number = -1;
+
+  openEditor(id: number){
+    this.editor_id = id
+    this.editor_input_names = this.website_components[id].input_names
+    this.editor_inputs = this.website_components[id].inputs
+    const editor = document.getElementById("editor");
+    if(editor){
+      editor.style.zIndex = "9999";
+    }
+  }
+
+  closeEditor(){
+    const editor = document.getElementById("editor");
+    if(editor){
+      editor.style.zIndex = "-1";
+    }
+  }
+
+  discardEditor(){
+    this.website_components.splice(this.editor_id, 1)
+    this.html_components.splice(this.editor_id, 1)
+    this.regenerate_html()
+    this.closeEditor()
+  }
+
+  onType(event: any, index: number){
+    this.editor_inputs[index] = event.target.value
+  }
+
+  imageProcessor(image: File) {
+    var reader = new FileReader();
+
+    reader.onload = (image: any) => {
+      console.log(image)
+      return image;
+    };
+
+    reader.onerror = (event: any) => {
+      console.log("File could not be read: " + event.target.error.code);
+    };
+  }
+
+  saveEditor(){
+    this.website_components[this.editor_id].inputs = this.editor_inputs
+    console.log(this.website_components[this.editor_id])
+    console.log(this.imageProcessor(this.website_components[this.editor_id]))
+    this.regenerate_html()
+    this.closeEditor()
   }
 }
